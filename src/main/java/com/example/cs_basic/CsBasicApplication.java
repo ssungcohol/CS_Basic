3b64024df3aa8e65e0c4637626256c465dc439b5
sungjae <sjhj825@naver.com>
2023-08-24 17:30:11 +0900

    Initial commit

---
+++ src/main/java/com/example/cs_basic/CsBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsBasicApplication.class, args);
+	}
+
+}
